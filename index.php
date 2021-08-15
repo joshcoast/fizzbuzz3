015cc6ef1cf6ea9a495a175e087735950db20833
Josh Coast <josh@joshcoast.com>
2021-08-15 14:36:22 -0600

    add step three enhancements

--- index.php
+++ index.php
@@ -2,32 +2,54 @@
 <html>
 <body>
 <h1>Fizz Buzz App</h1>
-<h2>Step 2</h2>
+<h2>Step 3</h2>
 
 <p>Output:</p>
-<p>
+<code>
 <?php
+
 $firstNumber = 1;
 $secondNumber = 20;
+$fizzTracker = 0;
+$buzzTracker = 0;
+$fizzbuzzTracker = 0;
+$luckyTracker = 0;
+$integerTracker = 0;
+
 foreach (range($firstNumber, $secondNumber) as $number) {
     if (strpos($number, '3') !== false ) {
         echo 'lucky ';
+        $luckyTracker++;
         continue;
     }
     if (0 !== $number % 3 && 0 !== $number % 5) {
         echo $number.' ';
+        $integerTracker++;
+        continue;
+    }
+    if (0 == $number % 3 && 0 == $number % 5) {
+        echo 'fizzbuzz ';
+        $fizzbuzzTracker++;
         continue;
     }
     if (0 === $number % 3) {
-        echo 'fizz';
+        echo 'fizz ';
+        $fizzTracker++;
     }
     if ( 0 === $number % 5) {
-        echo 'buzz';
+        echo 'buzz ';
+        $buzzTracker++;
     }
-    echo ' ';
 }
+echo "<br>fizz: " . $fizzTracker . "<br>";
+echo "buzz: " . $buzzTracker . "<br>";
+echo "fizzbuzz: " . $fizzbuzzTracker . "<br>";
+echo "lucky: " . $luckyTracker . "<br>";
+echo "integer: " . $integerTracker . "<br>";
+echo "(Integer is " . $integerTracker . " because there were " . $integerTracker . " numbers that were not altered in any way)."
 ?>
-</p>
+
+</code>
 
 </body>
 </html>
